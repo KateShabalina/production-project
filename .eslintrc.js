df05be48994d65c72719df429dac42e660032993
Kate Shabalina <letterforkatherine@gmail.com>
2023-01-20 20:49:23 -0800

    add stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'plugin:i18next/recommended',
   ],
   overrides: [],
   parser: '@typescript-eslint/parser',
@@ -18,6 +19,8 @@ module.exports = {
   },
   plugins: [
     'react',
+    '@typescript-eslint',
+    'i18next',
   ],
   rules: {
     'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
@@ -32,6 +35,7 @@ module.exports = {
     'import/extensions': 'off',
     'import/no-extraneous-dependencies': 'warn',
     'no-underscore-dangle': 'off',
+    'i18next/no-literal-string': ['error', { marcupOnly: true }],
   },
   globals: {
     __IS_DEV__: true,
