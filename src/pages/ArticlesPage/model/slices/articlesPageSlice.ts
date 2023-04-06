dc811a006f4526a202983fe0fa854352a4d834e5
Kate Shabalina <letterforkatherine@gmail.com>
2023-04-06 15:55:33 -0700

    fix acticlesPageSchema import

--- src/pages/ArticlesPage/model/slices/articlesPageSlice.ts
+++ src/pages/ArticlesPage/model/slices/articlesPageSlice.ts
@@ -5,8 +5,8 @@ import {
 } from 'entities/Article';
 import { ARTICLES_VIEW_LOCALSTORAGE_KEY } from 'shared/const/localstorage';
 import { SortOrder } from 'shared/types';
+import { ArticlesPageSchema } from 'pages/ArticlesPage';
 import { fetchArticlesList } from '../services/fetchArticlesList/fetchArticlesList';
-import { ArticlesPageSchema } from '../types/ArticlesPageSchema';
 
 const articlesAdapter = createEntityAdapter<Article>({
   selectId: (article) => article.id,
